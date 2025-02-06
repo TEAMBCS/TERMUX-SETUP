b8feeb7105587d5c5f8fb8cbe97500d353234463
Bangladesh Cyber Squad <147122723+TEAMBCS@users.noreply.github.com>
2025-02-06 21:33:54 +0600

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,35 @@
+clear
+echo "
+                   Termux  Full Command Setup
+
+  ____   _____  _____    _____ ______ _______ _    _ _____  
+ |  _ \ / ____|/ ____|  / ____|  ____|__   __| |  | |  __ \ 
+ | |_) | |    | (___   | (___ | |__     | |  | |  | | |__) |
+ |  _ <| |     \___ \   \___ \|  __|    | |  | |  | |  ___/   🇹🇪🇦🇲 🇧🇨🇸
+ | |_) | |____ ____) |  ____) | |____   | |  | |__| | |     
+ |____/ \_____|_____/  |_____/|______|  |_|   \____/|_|     | lolcat
+    
+                      🇧🇦🇳🇬🇱🇦🇩🇪🇸🇭 🇨🇾🇧🇪🇷 🇸🇶🇺🇦🇩 
+
+" | lolcat
+echo "=========================================" | lolcat
+echo "   Tools Name : Basic Pkg Install" | lolcat
+echo "   Author : Shawon Islam Saim" | lolcat
+echo "   Fb Group : Bangladesh Cyber Squad" | lolcat
+echo "   Github   : github.com/TEAMBCS" | lolcat
+echo " Frist press y And Enter if No run press y" | lolcat
+echo "=========================================="
+termux-setup-storage
+echo " Storage Permission Granted" | lolcat
+pkg update -y &&  pkg upgrade -y &&  pkg install python -y &&  pkg install python2 -y &&  pkg install fish -y && pkg install ruby -y && pkg install git -y &&  pkg install php -y &&  pkg install perl -y && pkg install nmap -y && pkg install bash -y && pkg install clang -y  && pkg install nano -y && pkg install w3m -y &&  pkg install figlet -y && pkg install cowsay -y && pkg install curl -y&& pkg install tar -y && pkg install zip -y && pkg install unzip -y && pkg install tor -y && pkg install wget -y && pkg install wcalc -y && pkg install bmon -y && pkg install golang -y && pkg install openssl -y && pkg install cmatrix -y && pkg install openssh -y && pkg install toilet && pkg install sl && pkg install vim && pkg install zsh && pkg install fortune && pkg install zsh && apt update && apt upgrade
+
+
+pip uninstall requests bs4
+pkg uninstall python
+pkg update 
+pkg upgrade
+pkg install python
+pip3 install requests bs4
+
+echo "All Package Installed" | lolcat
+echo " Stay with 🇹🇪🇦🇲 🇧🇨🇸Thanks" | lolcat
